3c0143aaa16fc5eb552c61f8e81640912f9ffb1e
Devon <defitjo16@gmail.com>
2018-04-07 16:15:23 -0400

    array analysis tests

--- array-analysis.js
+++ array-analysis.js
@@ -6,7 +6,7 @@ const avg = (arr) => {
   return total / arr.length;
 }
 
-const arrayAnalysis = arr => ({
+const arrayAnalysis = (arr) => ({
   min: Math.min(...arr),
   max: Math.max(...arr),
   length: arr.length,
